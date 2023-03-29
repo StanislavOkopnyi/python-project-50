6e02f86b376557815c41f30a0038c63ff5d6b408
Pineapple_Juice <st.okopnyi@gmail.com>
2023-03-29 17:11:02 +0700

    little changes for better reading

--- test/fixtures/compare_results/compare_dicts_results.py
+++ test/fixtures/compare_results/compare_dicts_results.py
@@ -1,26 +1,28 @@
-test_compare_dicts1 = [{'name': 'foo', 'last': '"bar"', 'now': '"bar"', 'type': 'item'},
-                       {'name': 'joo',
-                        'value': [
-                            {'name': 'noo',
-                             'last': '"mar"',
-                             'now': '"mar"',
-                             'type': 'item'},
-                            {'name': 'qoo',
-                             'last': '"__empty_value__"',
-                             'now': '"xar"',
-                             'type': 'item'},
-                            {'name': 'zoo',
-                             'last': '"xar"',
-                             'now': '"__empty_value__"',
-                             'type': 'item'}],
-                        'type': 'common_dict'},
-                       {'name': 'loo',
-                        'last': '"__empty_value__"',
-                        'now': [{'name': 'moo', 'last': '"dar"', 'now': '"dar"', 'type': 'item'}],
-                        'type': 'item'},
-                       {'name': 'roo', 'last': '"aar"', 'now': '"__empty_value__"', 'type': 'item'}]
+test_compare_dicts1 = \
+    [{'name': 'foo', 'last': '"bar"', 'now': '"bar"', 'type': 'item'},
+     {'name': 'joo',
+      'value': [
+          {'name': 'noo',
+           'last': '"mar"',
+           'now': '"mar"',
+           'type': 'item'},
+          {'name': 'qoo',
+           'last': '"__empty_value__"',
+           'now': '"xar"',
+           'type': 'item'},
+          {'name': 'zoo',
+           'last': '"xar"',
+           'now': '"__empty_value__"',
+           'type': 'item'}],
+      'type': 'common_dict'},
+     {'name': 'loo',
+      'last': '"__empty_value__"',
+      'now': [{'name': 'moo', 'last': '"dar"', 'now': '"dar"', 'type': 'item'}],
+      'type': 'item'},
+     {'name': 'roo', 'last': '"aar"', 'now': '"__empty_value__"', 'type': 'item'}]
 
-test_make_common_dict1 = {'name': 'foo',
-                          'value': [{'name': 'w', 'last': '"z"', 'now': '"w"', 'type': 'item'},
-                                    {'name': 'x', 'last': '"y"', 'now': '"y"', 'type': 'item'}],
-                          'type': 'common_dict'}
+test_make_common_dict1 = \
+    {'name': 'foo',
+     'value': [{'name': 'w', 'last': '"z"', 'now': '"w"', 'type': 'item'},
+               {'name': 'x', 'last': '"y"', 'now': '"y"', 'type': 'item'}],
+     'type': 'common_dict'}

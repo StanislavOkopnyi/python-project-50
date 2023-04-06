32482a98a67ac7821f0acfeff18c7b42433e193b
Pineapple_Juice <st.okopnyi@gmail.com>
2023-04-06 15:21:06 +0700

    Add default="stylish" to --format argument

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -4,7 +4,7 @@ import argparse
 parser = argparse.ArgumentParser(description='Compares two configuration files'
                                  ' and shows a difference.')
 parser.add_argument("-f", "--format", metavar="FORMAT",
-                    help="set format of output")
+                    help="set format of output", default="stylish")
 parser.add_argument("first_file")
 parser.add_argument("second_file")
 
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -3,13 +3,9 @@ from gendiff import generate_diff
 
 
 def main():
-    if args.format:
-        difference = generate_diff(args.first_file, args.second_file,
-                                   args.format)
-        print(difference)
-    else:
-        difference = generate_diff(args.first_file, args.second_file)
-        print(difference)
+    difference = generate_diff(args.first_file, args.second_file,
+                               args.format)
+    print(difference)
 
 
 if __name__ == "__main__":

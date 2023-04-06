26a297993ceefc9d40b616113ddd65ee5da8f258
Pineapple_Juice <st.okopnyi@gmail.com>
2023-04-06 14:47:26 +0700

    Test fix
    Tests had wrong arguments

--- test/test_generate_diff_json.py
+++ test/test_generate_diff_json.py
@@ -5,4 +5,4 @@ from gendiff.formatters.json import make_json
 def test_generate_dif():
     input_data = ("test/fixtures/json_yaml/file1.json",
                   "test/fixtures/json_yaml/file2.json")
-    assert generate_diff(*input_data, make_json)
+    assert generate_diff(*input_data, "json")
--- test/test_generate_diff_plain.py
+++ test/test_generate_diff_plain.py
@@ -11,7 +11,7 @@ def test_gendiff_path():
                    "test/fixtures/json_yaml/filepath2.json")
     expected = filepath1_filepath2_json
 
-    assert generate_diff(*input_data1, make_plain) == expected
+    assert generate_diff(*input_data1, "plain") == expected
 
 
 def test_gendiff_yaml():
@@ -20,13 +20,13 @@ def test_gendiff_yaml():
 
     expected1 = file1_file2_yaml
 
-    assert generate_diff(*input_data1, make_plain) == expected1
+    assert generate_diff(*input_data1, "plain") == expected1
 
     input_data2 = ("test/fixtures/json_yaml/file3.yaml",
                    "test/fixtures/json_yaml/file4.yaml")
     expected2 = file3_file4_yaml
 
-    assert generate_diff(*input_data2, make_plain) == expected2
+    assert generate_diff(*input_data2, "plain") == expected2
 
 
 def test_gendiff_json():
@@ -34,19 +34,19 @@ def test_gendiff_json():
                    "test/fixtures/json_yaml/file2.json")
     expected1 = file1_file2_json
 
-    assert generate_diff(*input_data1, make_plain) == expected1
+    assert generate_diff(*input_data1, "plain") == expected1
 
     input_data2 = ("test/fixtures/json_yaml/file3.json",
                    "test/fixtures/json_yaml/file4.json")
     expected2 = file3_file4_json
 
-    assert generate_diff(*input_data2, make_plain) == expected2
+    assert generate_diff(*input_data2, "plain") == expected2
 
     input_data3 = ("test/fixtures/json_yaml/file5.json",
                    "test/fixtures/json_yaml/file6.json")
     expected3 = file5_file6_json
 
-    assert generate_diff(*input_data3, make_plain) == expected3
+    assert generate_diff(*input_data3, "plain") == expected3
 
 
 def test_gendiff_json_empty():
@@ -54,10 +54,10 @@ def test_gendiff_json_empty():
                    "test/fixtures/json_yaml/file3_1.json")
     expected1 = file3_file3_1_json
 
-    assert generate_diff(*input_data1, make_plain) == expected1
+    assert generate_diff(*input_data1, "plain") == expected1
 
     input_data2 = ("test/fixtures/json_yaml/file3_1.json",
                    "test/fixtures/json_yaml/file3.json")
     expected2 = file3_1_file3_json
 
-    assert generate_diff(*input_data2, make_plain) == expected2
+    assert generate_diff(*input_data2, "plain") == expected2
